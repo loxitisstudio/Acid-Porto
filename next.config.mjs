75ab7150eeb7932cfd548283e2df4a7010b006e3
loxitis <rasitputra009@gmail.com>
2026-09-03 19:46:40 +0700

    Allow Cloudinary project thumbnails

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'imgur.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
 };
